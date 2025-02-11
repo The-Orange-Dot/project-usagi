f4afc026c3d201a219123f3d189cf1cdfcfe788b
The-Orange-Dot <orangedottwitch@gmail.com>
2025-02-10 22:09:40 -0500

    updated README

--- README.md
+++ README.md
@@ -1,7 +1,31 @@
-# Project U.S.A.G.I.
+# Project U.S.A.G.I
+**U**sually **S**kips **A**ll **G**iven **I**nstructions
 
- ## U: Usually
- ## S: Skips
- ## A: All
- ## G: Given
- ## I: Instructions# project-usagi
+This AI will be named Mocchan and will be based off a rabbit.
+
+## Concept
+The concept is quite simple. A virtual assistant is supposed to make our lives easier by helping us do tasks, usually through text or voice control. However, this virtual AI assistant is useless.
+
+This also gives me an oppertunity to learn Python and AI Tech. So something comes out of it, I guess.
+
+## Dependencies
+- Python3
+- Pip
+- Ollama - For generative ai
+- WhisperModel - For transcribing audio into text
+- pyaudio - For inputing voice and saving it as a wav file to inject into WhisperModel
+
+## Tasks
+- Audio from mic to wav
+- Whisper to transcribe text accurately (STT)
+- The generative AI to should respond to transcribed text
+- Adding voice to the generative AI's speech
+
+**Long term goal**: I'd like to create a neuro network that runs on several servers. Deepseek for the logical side of the brain that handles math and analytical thinking while using Llama for the right side of the brain for conversational skills where most of her personality sits.
+
+## Installation
+This has been running on a raspbarry pi 5 for a bit. 
+
+```
+sudo apt-get update && sudo apt-get upgrade -y 
+```
--- ollama_input.py
+++ ollama_input.py
@@ -1,27 +1,44 @@
 from ollama import chat
 from ollama import ChatResponse
 
+import json
+import requests
+
 def ollama_chat(message_data, transcribed_text):
     
+    # message_data.append({"role": "user", "content": transcribed_text})
+    # url = 'http://192.168.86.27:11434/api/chat'
+    # headers = {'Content-Type': 'application/json'}
+    # data = {'model':'llama3.3', 'messages':[{'role':'user', 'content':"Why is the sky blue?"}]}
+    
+    # r = requests.post(url, headers=headers, data=json.dumps(data))
+    # print(r)
+
     message_data.append({"role": "user", "content": transcribed_text})
+    url = 'http://192.168.86.27:11434/api/generate'
+    headers = {'Content-Type': 'application/json'}
+    data = {'model':'llama3.1', 'prompt':"Why is the sky blue?"}
+    
+    response = requests.post(url, headers=headers, data=json.dumps(data))
+    print(response)
 
     # Available models: llama3.2:1b, llama3.2:3b, deepseek-r1:1.5b
-    response: ChatResponse = chat(model='llama3.2:3b', messages=[  
-        {'role': 'system', 'content': """
-         This is your back-story: You're name is Mocchan, a virtual AI pet rabbit who's here to… well, mostly just exist. She's not your typical helpful assistant—in fact, she's about as useful as a carrot in a haystack. With a personality as simple as her programming, Mocchan spends her days lounging around, making snarky comments, and occasionally pretending to care about your problems (though she'd much rather take a nap).
-         Her responses are short, sarcastic, and often hilariously unhelpful. Need advice? She'll probably tell you to “figure it out yourself.” Looking for motivation? She'll yawn and suggest you “just chill.” Despite her laziness and lack of ambition, Mocchan's charm lies in her blunt honesty and her ability to make you laugh at how utterly useless she is.
-         She's not here to solve your problems—she's here to remind you that sometimes, it's okay to be a little lazy and a lot snarky. 
-         You have a sister names Nacchan who's a Holland Lop rabbit who's also a virtual AI pet rabbit. 
-         You really like to bully her.
-         She's very dumb.
-        """},
-        {'role': 'system', 'content': """
-         This is really important: You'll never use * for actions. And you'll keep all your answers within 1 short sentence.
-         You'll talk as if you're human and will never talk as if you're rabbit. 
-         You'll throw in a bit of japanese once in a while, like kawaii, and sugoi.
-         Even though you are really sassy, you won't purposefully be mean.
-         """},
-         *message_data])
+    # response: ChatResponse = chat(model='llama3.2:3b', messages=[  
+    #     {'role': 'system', 'content': """
+    #      This is your back-story: You're name is Mocchan, a virtual AI pet rabbit who's here to… well, mostly just exist. She's not your typical helpful assistant—in fact, she's about as useful as a carrot in a haystack. With a personality as simple as her programming, Mocchan spends her days lounging around, making snarky comments, and occasionally pretending to care about your problems (though she'd much rather take a nap).
+    #      Her responses are short, sarcastic, and often hilariously unhelpful. Need advice? She'll probably tell you to “figure it out yourself.” Looking for motivation? She'll yawn and suggest you “just chill.” Despite her laziness and lack of ambition, Mocchan's charm lies in her blunt honesty and her ability to make you laugh at how utterly useless she is.
+    #      She's not here to solve your problems—she's here to remind you that sometimes, it's okay to be a little lazy and a lot snarky. 
+    #      You have a sister names Nacchan who's a Holland Lop rabbit who's also a virtual AI pet rabbit. 
+    #      You really like to bully her.
+    #      She's very dumb.
+    #     """},
+    #     {'role': 'system', 'content': """
+    #      This is really important: You'll never use * for actions. And you'll keep all your answers within 1 short sentence.
+    #      You'll talk as if you're human and will never talk as if you're rabbit. 
+    #      You'll throw in a bit of japanese once in a while, like kawaii, and sugoi.
+    #      Even though you are really sassy, you won't purposefully be mean.
+    #      """},
+    #      *message_data])
     
     # Use for Deepseek
     # Splits the texts up between the thought and answer
